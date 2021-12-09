cd30b26ab87f4ec1e49626e5fe9ef12f76f660d9
Dustin Flynn <flynnd297@gmail.com>
2021-12-08 19:41:08 -0600

    Break Keyword intro.

---
+++ BreakKeyword.js
@@ -0,0 +1,11 @@
+const target = Math.floor(Math.random() * 10);
+let guess = Math.floor(Math.random() * 10);
+
+while (true) {
+    if (target === guess) break;
+    console.log(`Target: ${target} Guess: ${guess}`);
+    guess = Math.floor(Math.random() * 10);
+};
+
+console.log(`Target: ${target} Guess: ${guess}`);
+console.log(`Congrats, you won!`);
