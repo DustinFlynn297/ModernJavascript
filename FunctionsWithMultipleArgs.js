9d1d568e3bc58511545f854731dad3db5bc3baef
Dustin Flynn <flynnd297@gmail.com>
2021-12-10 21:15:08 -0600

    Adding functions with multiple parameters and arguments.

---
+++ FunctionsWithMultipleArgs.js
@@ -0,0 +1,20 @@
+function square(num) {
+    console.log(num * num);
+}
+// Squaring 6 will give us 36
+square(6);
+
+// defining a function with two parameters. When you call this function you must pass two arguments for the function to run.
+function sum (x,y) {
+    console.log(x + y);
+};
+
+// Sum should be 23
+sum(5,18);
+
+// defining a function to do simple division
+function division (a, b) {
+    console.log(a / b);
+};
+//10 divided by 5 should be 2
+division(10, 5)
