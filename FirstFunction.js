275ebc51bc375ae4a93421cbb24754cf5499de78
Dustin Flynn <flynnd297@gmail.com>
2021-12-10 20:56:36 -0600

    First function, learning to define and call a function.

---
+++ FirstFunction.js
@@ -0,0 +1,10 @@
+// defining a function
+
+function grumpus () {
+    console.log('ugh.. you again...');
+    console.log('FOR THE LAST TIME...');
+    console.log('LEAVE ME ALONE!!!!');
+};
+
+// calling a function
+grumpus();
