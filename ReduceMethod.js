cfb49b7ed9d6706039ff69d453a5176d5d00f60f
Dustin Flynn <flynnd297@gmail.com>
2021-12-18 22:24:51 -0600

    More reduce method examples.

--- ReduceMethod.js
+++ ReduceMethod.js
@@ -7,3 +7,24 @@ const nums = [3,4,5,6,7,8,9];
 const product = nums.reduce((total, currentVal) => {
     return total * currentVal;
 })
+
+const grades = [23, 67, 89, 97, 78, 94];
+
+//First way to get maxGrade using if statement
+const maxGrade = grades.reduce((max, currentVal) => {
+    if (currentVal > max) return currentVal;
+    return max;
+})
+
+//Second way to get maxGrade using Math.max
+const maxGrade2 = grades.reduce((max, currentVal) => {
+    return Math.max(max, currentVal);
+});
+const minGrade = grades.reduce((min, currentVal) => {
+    return Math.min(min, currentVal);
+});
+
+// can declare an initial value when using reduce directly in the function
+const sum = [10, 20, 30, 40, 50].reduce((sum, currVal) => {
+    return sum + currVal;
+})
