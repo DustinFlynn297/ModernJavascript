a5b3964aae9e4548d90a771735b96ebbd8807ee1
Dustin Flynn <flynnd297@gmail.com>
2021-12-21 22:03:29 -0600

    Shorthand for creating methods in objects.

--- Methods.js
+++ Methods.js
@@ -10,4 +10,17 @@ const math = {
     multiply: function(x,y){
         return x * y;
     }
+}   
+
+// can use an arrow function, but it's not that common because of the keyword "This"
+// There is also a shorthand where we don't have to use the key value pair syntax.
+
+const auth = {
+    username: 'TommyBot',
+    login(){
+        console.log("Logged you in!")
+    },
+    logout(){
+        console.log("Goodbye")
+    }
 }
