0628e6f402a76be3bfcbc89152620a75526d48fc
Dustin Flynn <flynnd297@gmail.com>
2021-12-25 13:21:56 -0600

    HTML and CSS check.

---
+++ Hoisting.js
@@ -0,0 +1,7 @@
+// gives undefined for first line because it runs animal as an undefined variable.
+console.log(animal)
+var animal = "Tapir";
+console.log(animal)
+
+let shrimp = "Harlequin Shrimp";
+console.log(shrimp)
